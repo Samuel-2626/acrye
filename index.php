1647888f2d4c8dd1b089f52402b88a6146e85f7b
Samuel Torimiro <efosonaname@gmail.com>
2022-10-15 12:32:29 +0100

    [add] index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ACRYE | African Center for Rural Youth Empowerment</title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="author" content="Dixon Torimiro, Torimiro, Professor Torimiro, Mojisola Torimiro, Mabel Odunsi, Lekan Dairo, Toyin Kolawole, acrye">
+    <meta name="description" content="A non-governmental organisation empowering the African youth for a decent livelihood">
+    <meta name="keywords" content="empowerment, rural youth, livelihood, african youth, acrye, scholarship, training, capacity building, farming, entreprenuership">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+  </head>
+  <body>
+
+  <div class="first-section">
+    <img src="images/logo.png" alt="Logo Background Image">
+    <h1>ACRYE</h1>
+    <p>African Center for Rural Youth Empowerment</p>
+  </div>
+
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
