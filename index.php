32e1a4598501aa6799bee35e3bd7dd58f62e40a1
Samuel Torimiro <efosonaname@gmail.com>
2022-10-22 05:05:07 +0100

    [add] section-section

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ACRYE | African Center for Rural Youth Empowerment</title>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="author" content="Dixon Torimiro, Torimiro, Professor Torimiro, Mojisola Torimiro, Mabel Odunsi, Lekan Dairo, Toyin Kolawole, acrye">
+    <meta name="author" content="Professor Dixon Olutade Torimiro">
     <meta name="description" content="A non-governmental organisation empowering the African youth for a decent livelihood">
     <meta name="keywords" content="empowerment, rural youth, livelihood, african youth, acrye, scholarship, training, capacity building, farming, entreprenuership">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
@@ -14,35 +14,33 @@
   </head>
   <body>
 
-  <div class="first-section">
+  <header class="first-section">
     <img src="images/logo.png" alt="Logo Background Image" class="mb-2">
-    <div class="first-section__main-text">
-        <h1>ACRYE</h1>
+    <section class="first-section__main-text">
+        <h1 class="first-section__main-text__heading">ACRYE</h1>
         <p>African Center for Rural Youth Empowerment</p>
-    </div>
-    <div class="first-section__secondary-text">
-        <h2>An NGO empowering the youth for a decent livelihood</h2>
-    </div>     
-  </div>
+    </section>
+  </header>
 
-  <div class="second-section">
+  <section class="second-section">
     <div class="container">
         <div class="row">
             <div class="col-md-6 mb-4">
-                African Center for Rural Youth Empowerment (ACRYE) was initiated and presented by Dixon Olutade Torimiro 
+                African Center for Rural Youth Empowerment (ACRYE) was initiated and presented by <mark>Dixon Olutade Torimiro</mark>  
                 as his country's action programme during his participation at the German Government/FAO's
                 sponsored 21st International Rural Youth Leaders Training Workshop held in Bavarian Farmers'
                 Union Training Center, Herrsching, Germany between June and July 2002.  
 
+                <section class="second-section__secondary-text">
                 <hr>
-
-                ACRYE was officially inaugurated in Nigeria on the 11th of November, 2004 at the Amphi 
-                Theatre, Obafemi Awolowo University (OAU), Ile-Ife, Osun State, Nigeria. Also, its maiden
-                series of Youth Empowerment Publication entitled: Ethical Revolution among Nigerian Youth
-                in Schools and Colleges was launched during the inauguration. The ceremony, which was widely
-                publicized by various media in Osun State, most especially, National Television Authority (NTA),
-                Ile-Ife, Osun State Broadcasting Corporation (OSBC), Osogbo and Tribune Newspaper, was well attended
-                by over 300 people of various classes including youth and adult. It was a very huge success!
+                    ACRYE was officially inaugurated in Nigeria on the 11th of November, 2004 at the Amphi 
+                    Theatre, Obafemi Awolowo University (OAU), Ile-Ife, Osun State, Nigeria. Also, its maiden
+                    series of Youth Empowerment Publication entitled: Ethical Revolution among Nigerian Youth
+                    in Schools and Colleges was launched during the inauguration. The ceremony, which was widely
+                    publicized by various media in Osun State, most especially, National Television Authority (NTA),
+                    Ile-Ife, Osun State Broadcasting Corporation (OSBC), Osogbo and Tribune Newspaper, was well attended
+                    by over 300 people of various classes including youth and adult. It was a very huge success!
+                </section>
 
             </div>
             <div class="col-md-6 mb-4">
@@ -50,7 +48,7 @@
             </div>
         </div>
     </div>
-  </div>
+  </section>
 
     
 
--- js/main.js
+++ js/main.js
@@ -1,30 +1,21 @@
 // Global colors
-var primaryColor = "#2c74b4";
-var secondaryColor = "#e0524e";
+const primaryColor = "#2c74b4";
+const secondaryColor = "#e0524e";
+const dark = "#000";
+const white = "#fff";
 
-let firstSection = document.querySelector(".first-section");
-let firstSectionMainText = document.querySelector(".first-section__main-text");
-let firstSectionSecondaryText = document.querySelector(".first-section__secondary-text");
+const firstSection = document.querySelector(".first-section");
 
 firstSection.addEventListener("mouseenter", function() {
     this.style.backgroundColor = primaryColor;
+    this.style.color = white
 })
 
 firstSection.addEventListener("mouseleave", function() {
     this.style.backgroundColor = secondaryColor;
+    this.style.color = dark;
 })
 
 
-setInterval(() => {
-    
-    if(firstSectionMainText.style.display === "block") {
-        firstSectionMainText.style.display = "none";
-    } else {
-        firstSectionMainText.style.display = "block";
-    }
-    
-}, 1000);
 
-// setInterval(() => {
-//     firstSectionSecondaryText.style.display = "block";
-// }, 2000);
+
