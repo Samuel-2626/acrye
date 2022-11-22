f081749c6db571e3461230a2ccfa326980789089
Samuel Torimiro <efosonaname@gmail.com>
2022-11-22 15:14:34 +0100

    first draft

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <meta name="keywords" content="empowerment, rural youth, livelihood, african youth, acrye, scholarship, training, capacity building, farming, entreprenuership">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
   </head>
   <body>
@@ -18,7 +20,7 @@
     <img src="images/logo.png" alt="Logo Background Image" class="mb-2">
     <section class="first-section__main-text">
         <h1 class="first-section__main-text__heading">ACRYE</h1>
-        <p>African Center for Rural Youth Empowerment</p>
+        <p><strong>African Center for Rural Youth Empowerment</strong></p>
     </section>
   </header>
   <div class="sub-heading text-center">
@@ -71,6 +73,310 @@
     </div>
   </section>
 
+  <section class="fourth-section">
+  <div class="container">
+  <h2 class="text-center fourth-section__heading mb-4">AIMS & OBJECTIVES</h2>
+    <div class="row">
+      <div class="col-md-6">
+      <ul>
+          <li>To develop and promote ideas and/or organize programmes and/or engage in activities
+            that will enhance adequate development of rural youth either solely or in collaboration 
+            with government agency, non governmental organizations, community based organizations, 
+            international and/or foreign agencies.
+          </li>
+          <li>
+            To sensitize and organise rural youth into groups for development and empowerment programmes.
+          </li>
+          <li>
+            To develop and promote talents among the rural youth.
+          </li>
+          <li>
+            To organize local, national, international learned conferences, seminars, 
+            workshops and awards focusing on the youth capacity building, and empowerment 
+            initiatives for full integration into the main stream of agricultural, rural 
+            and national economic development in Nigeria.
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-6 fourth-section--hide-on-small-device">
+        <ul>
+          <li>
+            To organise and sponsor local, national and international competitions, 
+            shows, exhibitions, holidays and exchange programmes for rural youth.
+          </li>
+          <li>
+            To organise training for rural youth on: exploration of rural resources, 
+            improved farm practices, poverty alleviation techniques, life enhancement and 
+            sex education, HIV/AIDS prevention and income generating/ entrepreneurial activities, 
+            information and communication management technologies etc.
+          </li>
+          <li>
+            To participate in and contribute to rural youth advancement activities 
+            at the local, national and international levels.
+          </li>
+          <li>
+            To conduct periodic documentation and keeping socio-economic data of 
+            rural youth situation in Nigeria.
+          </li>
+          <li>
+            To produce, source and circulate printed and non-printed materials 
+            related to the youth for the purpose of education and training.
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  </section>
+
+  <section class="fifth-section">
+    <div class="container">
+    <h2 class="text-center fourth-section__heading mb-3">PROGRAMMES</h2>
+      <div class="row">
+        <div class="col-md-6">
+          <h3 class="fifth-section__sub-heading">Talents & Entrepreneurship Development</h3>
+          <p>
+          This Programme is designed to discover the participant's hidden 
+          talents and develop such talents in a way that it will meet his or 
+          her entrepreneurship needs that will enable him or her to earn a decent 
+          and sustainable livelihood.
+          </p>
+        </div>
+        <div class="col-md-6 mb-5">
+        <img src="images/programmes-1.jpg" alt="Programmes One Image" class="img-fluid img-thumbnail">
+        </div>
+        <div class="col-md-6 mb-5 fifth-section--hide-on-small-devices">
+        <img src="images/programmes-2.jpg" alt="Programmes Two Image" class="img-fluid img-thumbnail">
+        </div>
+        <div class="col-md-6">
+        <h3 class="fifth-section__sub-heading">
+        Apprenticeship Support
+          </h3>
+          <p>
+          This Support offers opportunity to any rural youth that is 
+          interested in learning any locally available vocation or trade for
+          which he or she has no financial support. The participant will be 
+          enrolled as an apprentice and established in any vocation of his or her 
+          interest within the locality with the financial support of ACRYE.
+          </p>
+        </div>
+        <div class="col-md-6 mb-5 fifth-section--show-only-on-small-devices">
+        <img src="images/programmes-2.jpg" alt="Programmes Two Image" class="img-fluid img-thumbnail">
+        </div>
+        <div class="col-md-6">
+        <h3 class="fifth-section__sub-heading">Skills Acquisition Training</h3>
+          <p>
+          The Centre offers series of skills acquisition training programmes 
+          for capacity building and transformation using peer educators, 
+          experiential methodologies and interactive participatory approaches. 
+          The training offers exposition on exploration of rural resources, leadership 
+          development, group management techniques, improved farm practices, poverty 
+          alleviation techniques, life enhancement and sex education, HIV/AIDS prevention, 
+          income generating/ entrepreneurial activities, information and communication 
+          management technologies among
+          </p>
+        </div>
+        <div class="col-md-6 mb-5">
+        <img src="images/programmes-4.jpg" alt="Programmes Four Image" class="img-fluid img-thumbnail">
+        </div>
+        <div class="col-md-6 mb-5 fifth-section--hide-on-small-devices">
+        <img src="images/programmes-3.jpg" alt="Programmes Three Image" class="img-fluid img-thumbnail">
+        </div>
+        <div class="col-md-6">
+        <h3 class="fifth-section__sub-heading">Small Business Support Scheme</h3>
+          <p>
+          This Scheme aims at providing small credit facility to any participant 
+          (with no capacity to access credit from formal financial and commercial institutions)
+          with a small viable business for which he or she requires small credit support for 
+          expansion.
+          </p>
+        </div>
+        <div class="col-md-6 mb-5 fifth-section--show-only-on-small-devices">
+        <img src="images/programmes-3.jpg" alt="Programmes Three Image" class="img-fluid img-thumbnail">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="six-section">
+  <div class="container">
+  <h2 class="text-center fourth-section__heading">STRATEGIES</h2>
+
+        <div class="row">
+            <div class="col-md-4 six-section--hover-add-border-effect">
+                <p class="text-center">
+                    <i class="fa fa-group fa-5x" style="color: #e0524e;"></i>
+                </p>
+                <h4 class="text-center six-section--light-blue-color">Youth Groups' Formation</h4>
+
+
+                <p class="text-center">
+                The strategy is being explored to ensure sustainability 
+                of the various programmes mounted by the Centre. 
+                The strategy emphasizes the youths' organizational 
+                building and development by mobilizing and facilitating 
+                the youth that want to participate in the Centre's programmes 
+                to form themselves into groups in line with the Centre's guidelines.
+              </p>
+            </div>
+            <div class="col-md-4 six-section--hover-add-border-effect">
+                <p class="text-center">
+                    <i class="fa fa-handshake-o fa-5x" style="color: #e0524e;"></i>
+                </p>
+                <h4 class="text-center six-section--light-blue-color">Institutional Affiliation and Linkages</h4>
+                <p class="text-center">
+                This strategy is being explored to provide space for any 
+                youth who may be interested in the Centre's apprenticeship supports 
+                and scholarship awards. The Centre will be willing to sign a memorandum 
+                of understanding with any credible vocational centre that will be 
+                willing to admit any of its participants as an apprentice. 
+                It will also ensure a very strong linkage with any educational 
+                institution of its scholarship awardees to facilitate proper monitoring.
+              </p>
+            </div>
+            <div class="col-md-4 six-section--hover-add-border-effect">
+                <p class="text-center">
+                    <i class="fa fa-line-chart fa-5x" style="color: #e0524e;"></i>
+                </p>
+                <h4 class="text-center six-section--light-blue-color">Promoting Savings and Revolving Credits</h4>
+                <p class="text-center">
+                This strategy is to build the youth financial capacity through 
+                culture of saving in order to facilitate a financial pool for 
+                credit facility among the youth group.
+              </p>
+            </div>
+            <div class="col-md-4 six-section--hover-add-border-effect">
+                <p class="text-center">
+                    <i class="fa fa-graduation-cap fa-5x" style="color: #e0524e;"></i>
+                </p>
+                <h4 class="text-center six-section--light-blue-color">Training and Education for Empowerment</h4>
+                <p class="text-center">
+                This strategy is a major tool for ensuring attitudinal and 
+                behaviour change in the process of capacity building and 
+                youth empowerment. This is being explored through the use of 
+                multi media and communication resources (printed and electronics media), 
+                peer education and experiential methodologies. The Centre uses its Partners, 
+                Fellows and Volunteers as Programme Officers in implementing most of its programmes, 
+                while the resource persons are employed for training on a part time basis.                </p>
+            </div>
+          
+        </div>
+
+    </div>
+  </section>
+
+  <section class="seventh-section">
+    <div class="container">
+    <h2 class="text-center fourth-section__heading mb-4">FACILITIES</h2>
+
+   <div class="row">
+        <div class="col-md-4 mb-4">
+        <img src="images/facility-1.jpg" alt="Programmes Four Image" class="img-fluid img-thumbnail">
+        <em>Olajumoke Hall (Front View)</em>
+        </div>
+        <div class="col-md-4 mb-4">
+        <img src="images/facility-3.jpg" alt="Programmes Four Image" class="img-fluid img-thumbnail">
+        <em>Demonstration Concrete Pond for Training</em>
+        </div>
+        <div class="col-md-4 mb-4">
+        <img src="images/facility-4.jpg" alt="Programmes Four Image" class="img-fluid img-thumbnail">
+        <em>Demonstration Snairy for Training</em>
+        </div>
+        <div class="col-md-4 mb-4">
+        <img src="images/facility-5.jpg" alt="Programmes Four Image" class="img-fluid img-thumbnail">
+        <em>Demonstration Poultry for Training</em>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="eight-section">
+    <div class="container">
+    <h2 class="text-center fourth-section__heading mb-4">MEET THE TEAM</h2>
+      <div class="row">
+        <div class="col-md-2">
+        <img src="images/chairman.jpg" alt="Programmes Four Image" class="img-fluid img-thumbnail mb-2">
+        <em>Chairman | Professor D.O Torimiro</em>
+        <p class="mt-1">
+        Dixon Olutade Torimiro (PhD), a Professor of Agricultural 
+        Extension and Rural Sociology.
+        </p>
+        </div>
+        <div class="col-md-2">
+        <img src="images/moji.jpg" alt="Programmes Four Image" class="img-fluid img-thumbnail">
+     
+        <p>
+        Mojisola Helen Torimiro is a seasoned administrator.
+        </p>
+        </div>
+        <div class="col-md-2">
+        <img src="images/dairo.jpg" alt="Programmes Four Image" class="img-fluid img-thumbnail">
+     
+        <p>
+        Professor O.A Dairo is a Professor of Religious studies 
+        at Redeemers University(RUN).
+        </p>
+        </div>
+        <div class="col-md-2">
+        <img src="images/odunsi.jpg" alt="Programmes Four Image" class="img-fluid img-thumbnail">
+
+        <p>
+        Mrs Mabel Adenrele Odunsi is a seasoned educator.
+        </p>
+        </div>
+        <div class="col-md-3">
+        <img src="images/kolawole.jpg" alt="Programmes Four Image" class="img-fluid img-thumbnail">
+ 
+        <p>
+        Professor Oluwatoyin Dare Kolawole is a Professor of Rural 
+        Development at the Okavango Research Institute (ORI), University of 
+        Botswana (UB) in Southern Africa.
+        </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!-- TODO -->
+  <!-- <section class="ninth-section">
+    <div class="container">
+    <h2 class="text-center fourth-section__heading mb-4">BENEFICIARIES</h2>
+      <div class="row">
+        <div class="col-md-6">
+
+        </div>
+        <div class="col-md-6">
+          
+        </div>
+      </div>
+    </div>
+  </section> -->
+
+  <section class="eleventh-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+        <img src="images/contact.jpg" alt="Programmes Four Image" class="img-fluid img-thumbnail">
+        </div>
+        <div class="col-md-6 text-center">
+          <address>
+          Goshenland Villa, Opposite CAC Hill of Righteousness,
+          <br>
+          Seminary Area, Zone 1, Opa, P.O.Box 1134, Lagere, Ile-Ife, Nigeria.
+          <br>
+          E-mail: torimiro@gmail.com;
+          <br>
+          Mobile Phone: +234 - (0) - 814 911 0574
+          <br>
+          </address>
+          <img src="images/logo.png" alt="Logo Background Image" class="mb-2">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+  &copy; ACRYE <?php echo date('Y'); ?> All rights reserved
+  </footer>
     
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
